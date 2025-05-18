9b7b6d396ccb1c6233b2709dd278082fc85fe1d7
Dilakshan Pahitharan <it22297372@my.sliit.lk>
2025-05-18 12:01:13 +0530

    Add files via upload

---
+++ frontend/src/assets/assets/assets.js
@@ -0,0 +1,44 @@
+import BigOnion from './vegetables/BigOnion.webp'
+import Broccoli from './vegetables/Broccoli.webp'
+import Cabbage from './vegetables/Cabbage.webp'
+import Carrot from './vegetables/Carrot.webp'
+import Cauliflower from './vegetables/Cauliflower.webp'
+import Celery from './vegetables/Celery.webp'
+import Cucumber from './vegetables/Cucumber.webp'
+import Garlic from './vegetables/Garlic.webp'
+import Knokhol from './vegetables/Knokhol.webp'
+import Leeks from './vegetables/Leeks.webp'
+import NelumAla from './vegetables/Nelum-Ala.webp'
+import Potato from './vegetables/Potato.webp'
+import Pumpkin from './vegetables/Pumpkin.webp'
+import RocketLeaves from './vegetables/Rocket-Leaves.webp'
+import SaladLeaves from './vegetables/Salad-Leaves.webp'
+import SpringOnion from './vegetables/Spring-Onion.webp'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+
+export const assets = {
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    BigOnion,
+    Broccoli,
+    Cabbage,
+    Carrot,
+    Cauliflower,
+    Celery,
+    Cucumber,
+    Garlic,
+    Knokhol,
+    Leeks,
+    NelumAla,
+    Potato,
+    Pumpkin,
+    RocketLeaves,
+    SaladLeaves,
+    SpringOnion,
+}
+
+
